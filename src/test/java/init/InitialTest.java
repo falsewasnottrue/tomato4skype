d6dea78a5295bfc815d9243cf92e84dd56cd8edf
Rasmus Kuschel <rasmus.kuschel@immobilienscout24.de>
2015-02-16 11:57:07 +0100

    batman

---
+++ src/test/java/init/InitialTest.java
@@ -0,0 +1,12 @@
+package init;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class InitialTest {
+
+  @Test
+  public void test() throws Exception {
+    assertNull(null);
+  }
+}
