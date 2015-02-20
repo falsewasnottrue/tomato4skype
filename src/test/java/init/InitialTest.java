396a1a06f7c86475482ceb3d5a5f6ec7eecafa6c
Rasmus Kuschel <rasmus.kuschel@immobilienscout24.de>
2015-02-20 11:01:14 +0100

    skype dependency

--- src/test/java/init/InitialTest.java
+++ src/test/java/init/InitialTest.java
@@ -1,12 +1,23 @@
 package init;
 
+import com.skype.ChatMessage;
+import com.skype.ChatMessageAdapter;
+import com.skype.Skype;
+import com.skype.SkypeException;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class InitialTest {
 
   @Test
   public void test() throws Exception {
-    assertNull(null);
+
+    Skype.addChatMessageListener(new ChatMessageAdapter() {
+      @Override
+      public void chatMessageReceived(ChatMessage receivedChatMessage) throws SkypeException {
+        if (receivedChatMessage.getType().equals(ChatMessage.Type.SAID)) {
+          receivedChatMessage.getSender().send("I'm working. Please wait a moment.");
+        }
+      }
+    });
   }
 }
